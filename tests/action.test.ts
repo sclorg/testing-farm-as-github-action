cc5a5600967f1745093051424223e0f25b5ff410
Jan Macku <jamacku@redhat.com>
2023-11-29 17:09:26 +0100

    test: integration test for infra issues

--- tests/action.test.ts
+++ tests/action.test.ts
@@ -319,7 +319,84 @@ describe('Integration tests', () => {
 `);
   });
 
-  test.todo('Testing Farm infrastructure error');
+  test('Testing Farm infrastructure error', async () => {
+    setDefaultInputs();
+
+    // Mock required Action inputs
+    // api_key - A testing farm server api key
+    vi.stubEnv('INPUT_API_KEY', 'abcdef-123456');
+    // git_url - An url to the GIT repository
+    vi.stubEnv(
+      'INPUT_GIT_URL',
+      'https://github.com/sclorg/testing-farm-as-github-action'
+    );
+    // tmt_plan_regex - A tmt plan regex which will be used for selecting plans. By default all plans are selected
+    vi.stubEnv('INPUT_TMT_PLAN_REGEX', 'fedora');
+
+    // Mock Testing Farm API
+    vi.mocked(mocks.newRequest).mockImplementation(
+      async (_request: NewRequest, _strict: boolean) => {
+        return Promise.resolve({
+          id: '1',
+        });
+      }
+    );
+    vi.mocked(mocks.requestDetails)
+      .mockResolvedValueOnce({ state: 'new', result: null })
+      .mockResolvedValueOnce({ state: 'queued', result: null })
+      .mockResolvedValueOnce({ state: 'pending', result: null })
+      .mockResolvedValueOnce({ state: 'running', result: null })
+      .mockResolvedValueOnce({
+        state: 'infra-error',
+        result: null,
+      });
+
+    // Run action
+    const octokit = new Octokit({ auth: 'mock-token' });
+    const pr = await PullRequest.initialize(1, octokit);
+
+    try {
+      await action(pr);
+    } catch (error) {
+      expect(error).toMatchInlineSnapshot(`
+        {
+          "message": "Testing Farm test failed - No summary provided",
+          "url": "https://artifacts.dev.testing-farm.io/1",
+        }
+      `);
+    }
+
+    expect(mocks.TFError).toHaveBeenCalledOnce();
+
+    // Check if we have waited for Testing Farm to finish
+    expect(mocks.requestDetails).toHaveBeenCalledTimes(5);
+
+    // Test outputs
+    expect(process.env['OUTPUT_REQUEST_ID']).toMatchInlineSnapshot('"1"');
+    expect(process.env['OUTPUT_REQUEST_URL']).toMatchInlineSnapshot(
+      '"https://api.dev.testing-farm.io/requests/1"'
+    );
+
+    // First call to request PR details, next two calls for setting the status
+    expect(mocks.request).toHaveBeenCalledTimes(3);
+    expect(mocks.request).toHaveBeenLastCalledWith(
+      'POST /repos/{owner}/{repo}/statuses/{sha}',
+      {
+        context: 'Testing Farm - Fedora',
+        description: 'Build finished - Infra problems',
+        owner: 'sclorg',
+        repo: 'testing-farm-as-github-action',
+        sha: 'd20d0c37d634a5303fa1e02edc9ea281897ba01a',
+        state: 'failure',
+        target_url: 'https://artifacts.dev.testing-farm.io/1',
+      }
+    );
+
+    // Test summary
+    await assertSummary(`<h1>Testing Farm as a GitHub Action summary</h1>
+<table><tr><th>Compose</th><th>Arch</th><th>Infrastructure State</th><th>Test result</th><th>Link to logs</th></tr><tr><td>Fedora-latest</td><td>x86_64</td><td>Failed</td><td>failure</td><td>[https://artifacts.dev.testing-farm.io/1/pipeline.log](pipeline.log)</td></tr></table>
+`);
+  });
 
   test('Testing Farm request details timeout', async () => {
     setDefaultInputs();
