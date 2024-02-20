607d6165c0a1e71c77775813e1bc129d21b29180
Jan Macku <jamacku@redhat.com>
2024-02-20 16:21:52 +0100

    feat: add post step when action run was cancelled
    In post step, we will send cancel request to TF to cancel pending Test run.

---
+++ src/post.ts
@@ -0,0 +1,36 @@
+import { debug, getInput, notice } from '@actions/core';
+import TestingFarmAPI from 'testing-farm';
+
+import { PullRequest } from './pull-request';
+import { getTfArtifactUrl, getTfRequestId } from './state';
+
+async function post(pr: PullRequest): Promise<void> {
+  const tfInstance = getInput('api_url');
+  const tfRequestId = getTfRequestId();
+  const tfArtifactUrl = getTfArtifactUrl();
+
+  const api = new TestingFarmAPI(tfInstance);
+
+  // This should happen really rarely, but it's better to check
+  if (!tfRequestId) {
+    throw new Error('POST: Missing Testing Farm request id');
+  }
+
+  const request = {
+    api_key: getInput('api_key', { required: true }),
+  };
+
+  notice(`Cancelling Testing Farm request: ${tfRequestId}`);
+  await api.cancelRequest(tfRequestId, request, false);
+  debug(`Request ${tfRequestId} was cancelled`);
+
+  // Set status to success when the request was cancelled
+  // It's not a test failure, the request was cancelled by the user
+  await pr.setStatus(
+    'success',
+    'Testing Farm request was cancelled',
+    tfArtifactUrl ?? undefined
+  );
+}
+
+export default post;
