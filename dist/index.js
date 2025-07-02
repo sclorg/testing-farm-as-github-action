21ecccb2ed240dc63e5f0430ab8b2517f77a53cc
Petr "Stone" Hracek <phracek@redhat.com>
2025-07-02 15:59:24 +0200

    Update dists and yarn.lock
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- dist/index.js
+++ dist/index.js
@@ -55362,7 +55362,7 @@ var createTokenAuth = function createTokenAuth2(token) {
 
 
 ;// CONCATENATED MODULE: ./node_modules/@octokit/core/dist-src/version.js
-const version_VERSION = "6.1.5";
+const version_VERSION = "7.0.2";
 
 
 ;// CONCATENATED MODULE: ./node_modules/@octokit/core/dist-src/index.js
