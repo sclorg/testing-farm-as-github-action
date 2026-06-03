c2ddaef4d2a3260e4d7d533d85e81993f599a5a1
Jan Macku <jamacku@redhat.com>
2026-06-03 13:40:43 +0200

    build: use esbuild to build the action and update node version to 24

--- dist/action.d.ts
+++
@@ -1,3 +0,0 @@
-import { PullRequest } from './pull-request';
-declare function action(pr: PullRequest): Promise<void>;
-export default action;
--- dist/octokit.d.ts
+++
@@ -1,5 +0,0 @@
-import { Octokit } from '@octokit/core';
-declare const CustomOctokit: typeof Octokit & import("@octokit/core/types").Constructor<{}>;
-export type CustomOctokit = InstanceType<typeof CustomOctokit>;
-export declare function getOctokit(token: string): Octokit;
-export {};
