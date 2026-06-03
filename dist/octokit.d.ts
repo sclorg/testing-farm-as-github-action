5519c6701f9914baa11fca264ad67f78aaef5508
Jan Macku <jamacku@redhat.com>
2026-06-03 13:34:45 +0200

    deps: update dependencies

--- dist/octokit.d.ts
+++ dist/octokit.d.ts
@@ -1,5 +1,5 @@
 import { Octokit } from '@octokit/core';
-declare const CustomOctokit: typeof Octokit & import("@octokit/core/dist-types/types").Constructor<{}>;
+declare const CustomOctokit: typeof Octokit & import("@octokit/core/types").Constructor<{}>;
 export type CustomOctokit = InstanceType<typeof CustomOctokit>;
 export declare function getOctokit(token: string): Octokit;
 export {};
